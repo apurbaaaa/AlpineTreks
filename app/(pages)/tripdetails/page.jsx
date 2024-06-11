750472d7e3b414666382a695b8519299c9596b01
apurbaaaa <apurbakoirala33@gmail.com>
2024-06-11 11:39:47 +0545

    feat: tripdetails init

---
+++ app/(pages)/tripdetails/page.jsx
@@ -0,0 +1,15 @@
+import TripDetails from "@/components/layout/components/trip-details"
+import Header4 from "@/components/layout/header/Header4";
+import FooterFour from "@/components/layout/footers/FooterFour";
+import TopTreks from "@/components/homes/tours/Top10treks";
+export default function page(){
+    return(
+        <div>
+            <Header4 />
+            <TripDetails />
+            <TopTreks />
+            <FooterFour />
+        </div>
+        
+    )
+}
