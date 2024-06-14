0ec49dc3ec7312928e8f5a0e70cd4b99d489258e
apurbaaaa <apurbakoirala33@gmail.com>
2024-06-14 17:11:29 +0545

    fix: readmore routing

--- components/homes/banners/Welcome.jsx
+++ components/homes/banners/Welcome.jsx
@@ -1,39 +1,42 @@
 "use client"
+import React from "react";
+import { useRouter } from "next/navigation";
 
-import React from "react"
-import Link from "next/link"
+export default function Welcome() {
+    const router = useRouter();
 
+    const handleReadMoreClick = () => {
+        router.push("/");
+    };
 
-export default function Welcome(){
-
-    return(
-        <section className="layout-pt-xl  layout-pb-lg  ">
-            <div data-anim-wrap className="container">
+    return (
+        <section className="layout-pt-xl layout-pb-lg">
+            <div className="container">
                 <div className="row y-gap-20 justify-between align-items-center">
-
-                    <div data-aos="fade-up" className="col-lg-7">
+                    <div className="col-lg-7">
                         <h2 className="text-40 md:text-24 mb-10">Welcome to Mountain Trekking Nepal</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <p>
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                         <div className="col-md-4 mt-20">
-                        <Link href="/" >
-                            <button className="button -md -dark-1 bg-accent-1 text-white col-12">
+                            <button className="button -md -dark-1 bg-accent-1 text-white col-12" onClick={handleReadMoreClick}>
                                 Read More
                             </button>
-                        </Link>
-                                
-                                </div>
+                        </div>
                     </div>
 
-                    <div data-aos="fade-up" data-aos-delay="400" className="col-lg-5">
-                        
-                        <img src="https://www.mountainmarttreks.com/public/images/wel1.png" alt="image" className="rounded-12"/>
-                
+                    <div className="col-lg-5">
+                        <img
+                            src="https://www.mountainmarttreks.com/public/images/wel1.png"
+                            alt="Mountain Trekking Nepal"
+                            className="rounded-12"
+                        />
                     </div>
                 </div>
             </div>
         </section>
-    )
+    );
 }
