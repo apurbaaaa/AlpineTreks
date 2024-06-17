d7265333ae709725625ca116bb82f5b2c9fbb6c3
apurbaaaa <apurbakoirala33@gmail.com>
2024-06-17 16:23:59 +0545

    chore: minor changes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 'use client'
 import ScrollToTop from "@/components/common/ScrollToTop";
 import "../public/css/style.css";
-
 import { DM_Sans } from "next/font/google";
 import ScrollTopBehaviour from "@/components/common/ScrollTopBehavier";
 import Wrapper from "@/components/layout/Wrapper";
@@ -10,31 +9,32 @@ import { useState } from "react";
 import axios from "axios";
 import Header4 from "@/components/layout/header/Header4";
 import FooterFour from "@/components/layout/footers/FooterFour";
+
 const dmsans = DM_Sans({
   weight: ["400", "500", "700"],
   style: ["normal", "italic"],
   subsets: ["latin"],
   display: "swap",
 });
-// const inter = Inter({ subsets: ["latin"] });
+
+
 if (typeof window !== "undefined") {
   import("bootstrap");
 }
-
-
-
-
 
 export default function RootLayout({ children }) {
 
   const [dataSettings, setDataSettings] = useState(true)
   const [loading, setLoading] = useState(true)
+  const [favicon,setFavicon] = useState('');
+  const [error,setError] = useState(null);
 
 useEffect(()=>{
   const fetchData = async () => {
     try{
       const responseSettings = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/settings`);
       setDataSettings(responseSettings?.data)
+      setFavicon(response?.data?.site_favicon);
     }
     catch(error){
       setError(error)
@@ -45,22 +45,6 @@ useEffect(()=>{
   }; fetchData();
 },[])
 
-  const [favicon,setFavicon] = useState('');
-  const [error,setError] = useState(null);
-
-  useEffect(()=>{
-      const fetchData = async ()=>{
-        try{
-          const response = await axios.get('https://mountaintrekkingnepal.com/api/settings');
-          setFavicon(response?.data?.site_favicon);
-
-        }catch(err){
-          setError(err);
-          console.log(err)
-        }
-      };
-      fetchData();
-  },[])
 
   return (
     <html lang="en">
