61e50cf4e656c0292e3d8925027d703e193713e9
apurbaaaa <apurbakoirala33@gmail.com>
2024-06-21 12:09:58 +0545

    feat: Make enquiry impl

--- components/package-components/BookCard.jsx
+++ components/package-components/BookCard.jsx
@@ -1,17 +1,15 @@
-// components/package-components/BookCard.js
-import { Link as ScrollLink } from 'react-scroll';
 import React, { useState, useEffect } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+import Link from 'next/link';
+import MakeAnEnquiry from './MakeAnEnquiry';
 
 const BookCard = () => {
     const [isFixed, setIsFixed] = useState(false);
-
+    const [modalVisible, setModalVisible] = useState(false);
+    
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 70) {
-                setIsFixed(true);
-            } else {
-                setIsFixed(false);
-            }
+            setIsFixed(window.scrollY > 70);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -21,58 +19,76 @@ const BookCard = () => {
         };
     }, []);
 
+
+
+    const makeEnquiryOpen = () => {
+        setModalVisible(true);
+        console.log('Open')
+    }
+    
+
+
     return (
-        <div className="col-lg-4">
-            <div
-                className="d-flex js-pin-content"
-                style={{
-                    position: isFixed ? "fixed" : "static",
-                    top: isFixed ? "70px" : "auto",
-                }}
-            >
-                <div className="tourSingleSidebar">
-                    <div className="d-flex justify-content-between">
-                        <div className="d-flex items-center price mobile-none flex-column">
-                            <div className="fw-500">Days</div>
-                            <div className="text-20 fw-700">14</div>
-                        </div>
-                        <div className="d-flex items-center price mobile-none flex-column">
-                            <div className="fw-500">Save </div>
-                            <div className="text-20 fw-700 ml-10">$0</div>
-                        </div>
-                        <div className="d-flex items-center price flex-column">
-                            <div className="fw-500">
-                                Price <del className="d-none">$1700</del>
+        <>
+            <div className="col-lg-4">
+                <div
+                    className="d-flex js-pin-content"
+                    style={{
+                        position: isFixed ? "fixed" : "static",
+                        top: isFixed ? "70px" : "auto",
+                    }}
+                >
+                    <div className="tourSingleSidebar">
+                        <div className="d-flex justify-content-between">
+                            <div className="d-flex items-center price mobile-none flex-column">
+                                <div className="fw-500">Days</div>
+                                <div className="text-20 fw-700">14</div>
+                            </div>
+                            <div className="d-flex items-center price mobile-none flex-column">
+                                <div className="fw-500">Save </div>
+                                <div className="text-20 fw-700 ml-10">$0</div>
+                            </div>
+                            <div className="d-flex items-center price flex-column">
+                                <div className="fw-500">
+                                    Price <del className="d-none">$1700</del>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="button-book">
-                        {/* Use ScrollLink for smooth scrolling */}
-                        <ScrollLink
-                            to="departure"
-                            smooth={true}
-                            duration={500}
-                            className="button -md -dark-1 col-12 bg-accent-1 text-white mt-20"
-                        >
-                            Book Now
-                            <i className="icon-arrow-top-right ml-10"></i>
-                        </ScrollLink>
-                        <button className="button -md -dark-1 col-12 mt-20 border-1" id="modalBtn">
-                            Make an Enquiry
-                            <i className="icon-arrow-top-right ml-10"></i>
-                        </button>
-                    </div>
-                    <div className="points">
-                        <ul>
-                            <li>Instant Booking</li>
-                            <li>Secure Payments</li>
-                            <li>Flexibility</li>
-                            <li>No Hidden Costs</li>
-                        </ul>
+                        <div className="button-book">
+                            <ScrollLink
+                                to="departure"
+                                smooth={true}
+                                duration={500}
+                                className="button -md -dark-1 col-12 bg-accent-1 text-white mt-20"
+                            >
+                                Book Now
+                                <i className="icon-arrow-top-right ml-10"></i>
+                            </ScrollLink>
+                            <button
+                                className="button -md -dark-1 col-12 mt-20 border-1"
+                                onClick={() => makeEnquiryOpen()}
+                            >
+                                Make an Enquiry
+                                <i className="icon-arrow-top-right ml-10"></i>
+                            </button>
+                        </div>
+                        <div className="points">
+                            <ul>
+                                <li>Instant Booking</li>
+                                <li>Secure Payments</li>
+                                <li>Flexibility</li>
+                                <li>No Hidden Costs</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            {modalVisible && (
+                <MakeAnEnquiry />
+            )}
+
+        </>
     );
 };
 
---
+++ components/package-components/MakeAnEnquiry.jsx
@@ -0,0 +1,166 @@
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+export default function MakeAnEnquiry() {
+    const [modalVisible, setModalVisible] = useState(true);
+    const [formData, setFormData] = useState({
+        fullName: '',
+        email: '',
+        contactNumber: '',
+        address: '',
+        numberOfPeople: '',
+        tripStartDate: '',
+        comment: '',
+        agree: false
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;    
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: type === 'checkbox' ? checked : value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission, e.g., send data to an API
+        console.log(formData);
+    };
+
+    const makeEnquiryClose = () => {
+        setModalVisible(false);
+        console.log("close");
+    }
+
+    return (
+        <section>
+            {modalVisible && (
+                <div className="modal-container" id="myModal" style={{display : 'block'}}>
+                    <div className="modal-content">
+                        <div className="modal-header text-center">
+                            <h2 className="modal-title">Make an Enquiry</h2>
+                            <p>Have a question before you book? Send your enquiry below and our travel experts will get back with the necessary answer(s).</p>
+                            <button type="button" className="close" id="closeModal" onClick={makeEnquiryClose}>&times;</button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleSubmit}>
+                                <div className="row y-gap-30 contactForm pt-30">
+                                    <div className="col-md-6">
+                                        <div className="form-input">
+                                            <input
+                                                type="text"
+                                                name="fullName"
+                                                required
+                                                value={formData.fullName}
+                                                onChange={handleChange}
+                                            />
+                                            <label className="lh-1 text-16 text-light-1">Full Name</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-input">
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                required
+                                                value={formData.email}
+                                                onChange={handleChange}
+                                            />
+                                            <label className="lh-1 text-16 text-light-1">Email</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-input">
+                                            <input
+                                                type="text"
+                                                name="contactNumber"
+                                                required
+                                                value={formData.contactNumber}
+                                                onChange={handleChange}
+                                            />
+                                            <label className="lh-1 text-16 text-light-1">Contact Number</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-input">
+                                            <input
+                                                type="text"
+                                                name="address"
+                                                required
+                                                value={formData.address}
+                                                onChange={handleChange}
+                                            />
+                                            <label className="lh-1 text-16 text-light-1">Address</label>    
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-input">
+                                            <input
+                                                type="number"
+                                                name="numberOfPeople"
+                                                required
+                                                value={formData.numberOfPeople}
+                                                onChange={handleChange}
+                                            />
+                                            <label className="lh-1 text-16 text-light-1">No Of People</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-input">
+                                            <input
+                                                type="date"
+                                                name="tripStartDate"
+                                                required
+                                                value={formData.tripStartDate}
+                                                onChange={handleChange}
+                                            />
+                                            <label className="lh-1 text-16 text-light-1">Trip Start Date</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-12">
+                                        <div className="form-input">
+                                            <textarea
+                                                name="comment"
+                                                value={formData.comment}
+                                                onChange={handleChange}
+                                            ></textarea>
+                                            <label className="lh-1 text-16 text-light-1">Enter text here...</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <div className="row y-gap-20 items-center justify-between">
+                                            <div className="col-auto">
+                                                <div className="policy">
+                                                    <input
+                                                        type="checkbox"
+                                                        id="signup-check"
+                                                        name="agree"
+                                                        className="custom-checkbox"
+                                                        checked={formData.agree}
+                                                        onChange={handleChange}
+                                                    />
+                                                    <label htmlFor="signup-check">
+                                                        By proceeding, you accept our
+                                                        <Link href="/legal" target="_blank" rel="noopener"> terms of use</Link> and 
+                                                        <Link href="/info/data-policy" target="_blank" rel="noopener"> data policy</Link>.
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div className="col-md-auto col-12">
+                                                <button type="submit" className="button -md -dark-1 bg-accent-1 text-white col-12">
+                                                    Submit <i className="icon-arrow-top-right text-16 ml-10"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </section>
+    );
+}
