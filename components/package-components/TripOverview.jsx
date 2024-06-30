02b97d7b5c0129be1f865c06516608852e27e20a
apurbaaaa <apurbakoirala33@gmail.com>
2024-06-30 16:15:05 +0545

    fix: readmore TripOverview

--- components/package-components/TripOverview.jsx
+++ components/package-components/TripOverview.jsx
@@ -1,16 +1,18 @@
-"use client";
 import React from "react";
 import DOMPurify from "dompurify";
-import { useState } from "react";
 import Link from "next/link";
 
 export default function TripOverview({ data }) {
-  const ParagraphLimit = 4;
-  const [isExpanded, setIsExpanded] = useState(false); // State to track full description visibility
+  const unpurified_overview = data.overview;
+  const updatedHtmlOverview = DOMPurify.sanitize(unpurified_overview);
 
-  const toggleExpand = (event) => {
-    event.preventDefault();
-    setIsExpanded(!isExpanded);
+  const unpurified_highlights = data.highlights;
+  const updatedHtmlHighlights = DOMPurify.sanitize(unpurified_highlights);
+
+  const ParagraphLimit = 3;
+
+  const slugType = (slug) => {
+    return slug === "nepal" ? "activity" : "package";
   };
 
   // Function to split the HTML content into paragraphs
@@ -26,35 +28,47 @@ export default function TripOverview({ data }) {
   };
 
   // Split HTML content into paragraphs
-  const paragraphs = splitIntoParagraphs(data.overview);
+  const paragraphs = splitIntoParagraphs(updatedHtmlOverview);
 
-  const sanitizedOverview = DOMPurify.sanitize(data.overview);
-  const sanitizedHighlights = DOMPurify.sanitize(data.highlights);
+  const handleToggleExpand = (event) => {
+    event.preventDefault();
+    const detailElement = event.target.closest(".trip-detail");
+    const isExpanded = detailElement.classList.toggle("expanded");
+    const overviewDiv = detailElement.querySelector("#overview-content");
+
+    if (isExpanded) {
+      overviewDiv.innerHTML = updatedHtmlOverview;
+      event.target.innerText = "Read Less";
+    } else {
+      overviewDiv.innerHTML = truncateParagraphs(paragraphs, ParagraphLimit);
+      event.target.innerText = "Read More";
+    }
+  };
 
   return (
     <div>
       <div className="trip-detail tour_details_boxed" id="overview">
         <h2>Trip Overview</h2>
+        <div>
+          <div
+            id="overview-content"
+            dangerouslySetInnerHTML={{
+              __html: truncateParagraphs(paragraphs, ParagraphLimit),
+            }}
+          ></div>
+          {paragraphs.length > ParagraphLimit && (
+            <Link href="#" onClick={handleToggleExpand}>
+              Read More
+            </Link>
+          )}
+        </div>
         <div className="highlight">
           <h2>Highlights of the Annapurna Base Camp Trek</h2>
-          <div>
-            {!isExpanded ? (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: truncateParagraphs(paragraphs, ParagraphLimit),
-                }}
-              ></div>
-            ) : (
-              // Render all paragraphs when expanded
-              <div dangerouslySetInnerHTML={{ __html: sanitizedOverview }}></div>
-            )}
-            {/* Show "Read More" link if more than two paragraphs */}
-            {paragraphs.length > ParagraphLimit && (
-              <Link href="#" onClick={toggleExpand}>
-                {isExpanded ? "Read Less" : "Read More"}
-              </Link>
-            )}
-          </div>
+          <p>
+            <span
+              dangerouslySetInnerHTML={{ __html: updatedHtmlHighlights }}
+            ></span>
+          </p>
         </div>
       </div>
     </div>
